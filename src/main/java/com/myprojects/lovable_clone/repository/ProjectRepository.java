438858795a665f25d6b57df8fd1c08d64cbd5441
mukullFromMac <mukulsavita0108@gmail.com>
2026-04-17 16:30:38 +0530

    Only return the project for which user is having access

--- src/main/java/com/myprojects/lovable_clone/repository/ProjectRepository.java
+++ src/main/java/com/myprojects/lovable_clone/repository/ProjectRepository.java
@@ -26,6 +26,11 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
             SELECT p FROM Project p
             WHERE p.id = :projectId
                 AND p.deletedAt IS NULL
+                AND EXISTS (
+                    SELECT 1 FROM ProjectMember pm
+                    WHERE pm.id.userId = :userId
+                    AND pm.id.projectId = :projectId
+                )
             """)
     Optional<Project> findAccessibleProjectByIdAndUserId(@Param("projectId") Long projectId,
                                                         @Param("userId") Long userId);
